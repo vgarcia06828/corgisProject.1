485b86d6e42a2d598ef518c1296868c4d594cf57
vigarcia <vigarcia828@gmail.com>
2024-04-04 13:45:15 -0700

    akjdhaksjd

---
+++ corgisData.py
@@ -0,0 +1,2 @@
+"""note: remember to make a return that says wether or not the chosen game is the sequel to another game (hint: it should respond with true or false)"""
+"""add what the producers are, and who produced them. """
